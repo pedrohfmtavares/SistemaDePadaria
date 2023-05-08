96fc910b586bb202bf31b2318c61a4c66fd5ee9c
Pedro Henrique Tavares <pedrohtavares11@gmail.com>
2023-05-08 13:13:08 -0300

    comentando o código

--- padaria.py
+++ padaria.py
@@ -1,18 +1,27 @@
+# IMPORTANDO A BIBLIOTECA TIME
 import time
 
+
+# ESTOQUES INICIAIS DOS PRODUTOS
 estoqueQueijo = 50.0
 estoquePao = 50.0
 estoqueFarinhaDeTrigo = 20
 
 
-queijoPreco = 24.90
-farinhaDeTrigoPreco = 6.99
+# LISTAS DE PRODUTOS DISPONÍVEIS
 produtosUN = ["farinha de trigo"]
 produtosKG = ["pao", "queijo"]
 
 
+# PREÇOS DOS PRODUTOS
+precoKG = 24.90
+farinhaDeTrigoPreco = 6.99
+
+
+# STRINGS PARA MENSAGENS ÚTEIS
 espacoEmBranco = " "
 sucesso = "Venda registrada com sucesso!"
+
 
 
 precosKG = {produtosKG[0]: estoquePao, produtosKG[1]: estoqueQueijo}
@@ -23,16 +32,17 @@ valoresKG = precosKG.values()
 valoresUN = precosUN.values()
 
 
+#VARIÁVEIS ÚTEIS
 dinheiro = 0
 faturamento = 0
 
 
-
+# LOOP PRINCIPAL DO CAIXA
 while True:
     registraProduto = input('Qual produto deseja registrar para a venda? \n')
     if registraProduto in produtosKG:
         registraQuilo = float(input(f'Quantos quilogramas deseja registrar para a venda? \n '))
-        dinheiro = registraQuilo * queijoPreco
+        dinheiro = registraQuilo * precoKG
         faturamento += dinheiro 
         if registraProduto == "pao":
             precosKG.update({produtosKG[0]: estoquePao - registraQuilo})
@@ -53,7 +63,7 @@ while True:
     else:
         print("Esse produto não existe ou não está disponível na nossa loja")
         print(espacoEmBranco)
-
+    # LOOP INTERNO PARA REINICIAR O CAIXA
     while True:
         voltar = input('Para registrar um novo produto,digite "y",para fechar o caixa,digite "n" ')
 
@@ -66,6 +76,8 @@ while True:
     if voltar == "n" or voltar == "N":
         break
 
+
+# FECHANDO O CAIXA
 tempo = 3
 print(f'Fechando o caixa em {tempo} segundos... ')
 tempo -= 1
@@ -79,6 +91,7 @@ print(f'Fechando o caixa em {tempo} segundo... ')
 time.sleep(1)
 
 
+# MENSAGENS FINAIS APÓS O FECHAMENTO DO CAIXA
 print(f'O faturamento total do dia foi de {faturamento:,.2f}R$')
 print(espacoEmBranco)
 print(f'O nosso estoque contém {estoquePao}KG de pao, {estoqueQueijo}KG de queijo e {estoqueFarinhaDeTrigo} unidades de farinha de trigo ')
